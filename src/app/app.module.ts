d39571de0949362cdc77b571737c7dddf1a6b67f
SADEWAMEERAGOPAL <SADEWADMEERA76@GMAIL.COM>
2026-01-15 20:02:20 +0530

    Create and Read todo functionality implemented

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,37 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
+import { TodoDashboardComponent } from './components/todo-dashboard/todo-dashboard.component';
+import { TodoFormComponent } from './components/todo-form/todo-form.component';
+import { TodoListComponent } from './components/todo-list/todo-list.component';
+import { MatconfiramationComponent } from './components/matconfiramation/matconfiramation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoDashboardComponent,
+    TodoFormComponent,
+    TodoListComponent,
+    MatconfiramationComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatIconModule,
+    MatDialogModule,
+    MatSnackBarModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
